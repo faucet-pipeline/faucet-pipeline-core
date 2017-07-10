cd15cac52295fcf3fa26ee86ed5e064da81e762b
FND <fnd@localhost.localdomain>
2017-07-10 17:16:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let makeWatcher = require("nite-owl");
+let path = require("path");
+
+const PLUGINS = {
+	js: "faucet-pipeline-js",
+	css: "faucet-pipeline-css"
+};
+
+module.exports = (rootDir, config = "faucet.js", // eslint-disable-next-line indent
+		{ watch, suppressFingerprinting, compact }) => {
+	let configPath = path.resolve(rootDir, config);
+	let configDir = path.dirname(configPath);
+	config = require(configPath);
+
+	let watcher = watch && makeWatcher(rootDir, 100); // XXX: magic number; configurable?
+
+	Object.keys(PLUGINS).forEach(type => {
+		let cfg = config[type];
+		if(!cfg) {
+			return;
+		}
+
+		let plugin = load(PLUGINS[type]);
+		plugin(cfg, { rootDir, configDir }, // eslint-disable-next-line indent
+				{ watcher, suppressFingerprinting, compact });
+	});
+};
+
+function load(pkg) {
+	try {
+		return require(pkg);
+	} catch(err) {
+		console.error(`ERROR: missing plugin - please install ${pkg}`);
+		process.exit(1);
+	}
+}
