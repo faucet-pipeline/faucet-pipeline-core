6abd99b9df21079039988828de5df21ad252ff9f
FND <fnd@localhost.localdomain>
2018-01-21 17:37:08 +0100

    added shell utilities for plugin tests
    as suggested by @moonglum, this was extracted from plugins' repositories

---
+++ test/cli_harness.sh
@@ -0,0 +1,54 @@
+set -euo pipefail
+
+# enters test directory
+function begin {
+	test_dir="${1:?}"
+
+	echo; echo `basename "$test_dir"`
+	pushd "$test_dir" > /dev/null
+}
+
+# returns to previous directory and removes artifacts directory
+function end {
+	artifacts_dir=${1:-"./dist"}
+
+	rm -r "$artifacts_dir"
+	popd > /dev/null
+}
+
+# compares two JSON files
+function assert_json {
+	actual="${1:?}"
+	expected="${2:?}"
+
+	json-diff "$expected" "$actual" || \
+			fail "files \`$actual\` and \`$expected\` are not equivalent"
+}
+
+# compares two files
+function assert_identical {
+	actual="${1:?}"
+	expected="${2:?}"
+
+	# NB: `git diff` provides colorization (dependent on configuration)
+	git diff --no-index "$expected" "$actual" || \
+			fail "files \`$actual\` and \`$expected\` are not identical"
+}
+
+# ensures that the given file does not exist
+function assert_missing {
+	filepath="${1:?}"
+
+	if [ -f "$filepath" ]; then
+		fail "file \`$filepath\` should not exist"
+	else
+		true
+	fi
+}
+
+function fail {
+	msg="${1:?}"
+
+	echo; echo "FAILURE: $msg"
+	false
+}
