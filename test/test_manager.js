3142f2eb8c3b32bb78ef83b177f613637493d3a6
FND <fnd@localhost.localdomain>
2018-02-15 07:41:41 +0100

    ensured path resolution on legacy Node supports local modules
    note that path resolution for third-party packages currently fails on
    Node v8 if CWD is not the same as `referenceDir`:
    https://github.com/nodejs/node/issues/18686

---
+++ test/test_manager.js
@@ -0,0 +1,39 @@
+/* global describe, before, after, it */
+"use strict";
+
+let AssetManager = require("../lib/manager");
+let path = require("path");
+let assert = require("assert");
+
+let assertSame = assert.strictEqual;
+
+describe("asset manager", _ => {
+	let cwd;
+
+	before(() => {
+		cwd = process.cwd();
+	});
+
+	after(() => {
+		process.chdir(cwd);
+	});
+
+	it("resolves file paths for local modules and third-party packages", () => {
+		let root = path.resolve(__dirname, "fixtures");
+		process.chdir(root);
+
+		let { resolvePath } = new AssetManager(root);
+
+		let filepath = resolvePath("dummy/src.js");
+		assertSame(path.relative(root, filepath), "dummy/src.js");
+
+		filepath = resolvePath("dummy/pkg.js");
+		assertSame(path.relative(root, filepath), "node_modules/dummy/pkg.js");
+
+		// local modules take precedence over third-party packages
+		["dummy", "dummy/index", "dummy/index.js"].forEach(module => {
+			let filepath = resolvePath(module);
+			assertSame(path.relative(root, filepath), "dummy/index.js");
+		});
+	});
+});
