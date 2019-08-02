0b76a6465e13f18e5061ff07b23368aab185bc55
FND <fnd@localhost.localdomain>
2019-08-02 11:26:05 +0200

    removed support for implicit local modules
    `require.resolve` behavior was changed in Node v12 to comply with Node
    resolution algorithm (see https://github.com/nodejs/node/issues/29139) -
    not sure why we thought this behavior was desirable in the first place
    (introduced in 3142f2eb8c3b32bb78ef83b177f613637493d3a6 - probably
    cargo-culting `require.resolve`'s observed behavior)
    
    however, on legacy Node versions behavior appears unchanged if the
    current working directory is the same as `rootDir` - I didn't bother
    enforcing modern behavior because legacy isn't worth the effort
    
    in the process, fixed legacy detection

--- test/test_manager.js
+++ test/test_manager.js
@@ -7,32 +7,45 @@ let assert = require("assert");
 
 let assertSame = assert.strictEqual;
 
+let [major] = process.version.substr(1).split(".").map(i => parseInt(i, 10));
+let MODERN = major >= 12; // legacy doesn't throw due to working directory
+
 describe("asset manager", _ => {
 	let root = path.resolve(__dirname, "fixtures");
 	let cwd;
+	let { exit } = process;
 
 	before(() => {
 		cwd = process.cwd();
 		process.chdir(root);
+		process.exit = code => {
+			throw new Error(`exit ${code}`);
+		};
 	});
 
 	after(() => {
 		process.chdir(cwd);
+		process.exit = exit;
 	});
 
-	it("resolves file paths for local modules and third-party packages", () => {
+	it("resolves file paths for third-party packages", () => {
 		let { resolvePath } = new AssetManager(root);
 
-		let filepath = resolvePath("dummy/src.js");
-		assertSame(path.relative(root, filepath), "dummy/src.js");
-
-		filepath = resolvePath("dummy/pkg.js");
+		let filepath = resolvePath("dummy/pkg.js");
 		assertSame(path.relative(root, filepath), "node_modules/dummy/pkg.js");
 
-		// local modules take precedence over third-party packages
-		["dummy", "dummy/index", "dummy/index.js"].forEach(module => {
-			let filepath = resolvePath(module);
-			assertSame(path.relative(root, filepath), "dummy/index.js");
-		});
+		filepath = resolvePath("./dummy/src.js");
+		assertSame(path.relative(root, filepath), "dummy/src.js");
+
+		if(MODERN) {
+			assert.throws(() => {
+				resolvePath("dummy/src.js");
+			}, /exit 1/);
+
+			["dummy", "dummy/index", "dummy/index.js"].forEach(module => {
+				let filepath = resolvePath(module);
+				assertSame(path.relative(root, filepath), "node_modules/dummy/index.js");
+			});
+		}
 	});
 });
