1174e8aae1988ddc62c744733972223aa39e9441
Lucas Dohmen <lucas@dohmen.io>
2025-03-09 13:06:17 +0100

    cleaned up
    This is a breaking change.
    
    * FileFinder is removed (pending based on the static/images work)
    * resolvePath is moved into util/index.js (this is a breaking change
      for aiur)
    * generateFingerprint is moved into lib/manager.js as it is the
      only place where it is used (the test is moved accordingly)
    * SerializedRunner is moved to lib, as it is only used in core

--- test/test_manager.js
+++ test/test_manager.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let { AssetManager } = require("../lib/manager");
+let { AssetManager, generateFingerprint } = require("../lib/manager");
 let { describe, it, before, after } = require("node:test");
 let path = require("path");
 let assert = require("assert");
@@ -49,5 +49,20 @@ describe("asset manager", () => {
 		assert.throws(() => {
 			resolvePath("dummy/videos");
 		}, /exit 1/);
+	});
+});
+
+describe("fingerprinting", () => {
+	it("generates a content-dependent hash", () => {
+		let fingerprint = generateFingerprint("/path/to/foo.js", "lorem ipsum");
+		assertSame(fingerprint, "/path/to/foo-80a751fde577028640c419000e33eba6.js");
+
+		fingerprint = generateFingerprint("/path/to/bar.js", "dolor sit amet");
+		assertSame(fingerprint, "/path/to/bar-7afed6210e0b8fce023f06abd4490fa0.js");
+	});
+
+	it("supports files without extension", () => {
+		let fingerprint = generateFingerprint("/path/to/baz", "lipsum");
+		assertSame(fingerprint, "/path/to/baz-8047cfaac755e5c7f77af066123980a5");
 	});
 });
